a4a2aad88e86349aa4658df2e1df7abbcfdd582d
alke_glb <alke_glb@hotmail.com>
2021-08-25 23:02:37 +0800

    Redis整合

--- service-core/src/main/java/com/alke/hfs/core/service/impl/DictServiceImpl.java
+++ service-core/src/main/java/com/alke/hfs/core/service/impl/DictServiceImpl.java
@@ -9,7 +9,10 @@ import com.alke.hfs.core.service.DictService;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.TimeoutUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +21,7 @@ import java.beans.Transient;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -31,14 +35,27 @@ import java.util.List;
 @Service
 public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
 
+    @Resource
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 数据导入
+     *
+     * @param inputStream
+     */
 //    设置事务回滚
     @Transactional(rollbackFor = Exception.class)
     @Override
     public void importData(InputStream inputStream) {
-        EasyExcel.read(inputStream, ExcelDictDTO.class,new ExcelDictDTOListener(baseMapper)).sheet().doRead();
+        EasyExcel.read(inputStream, ExcelDictDTO.class, new ExcelDictDTOListener(baseMapper)).sheet().doRead();
         log.info("Excel导入成功");
     }
 
+    /**
+     * 数据导出
+     *
+     * @return
+     */
     @Override
     public List<ExcelDictDTO> listDictData() {
         List<Dict> dictList = baseMapper.selectList(null);
@@ -51,10 +68,29 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
         return excelDictDTOList;
     }
 
+    /**
+     * 展示页面数据
+     *
+     * @param parentId
+     * @return
+     */
     @Override
     public List<Dict> listByParentId(Long parentId) {
+
+//        先在redis中查询数据，若查到了 则返回，否则去数据库中查找并写入redis中
+        try {
+            List<Dict> dictList = (List<Dict>) redisTemplate.opsForValue().get("hfs:core:dictList:" + parentId);
+            if (dictList!=null){
+                log.info("从Redis中获取数据列表");
+                return dictList;
+            }
+        } catch (Exception e) {
+            log.error("Redis服务器异常:"+ ExceptionUtils.getStackTrace(e));
+        }
+//        在数据库中查找
+        log.info("从数据库中获取数据列表");
         QueryWrapper<Dict> dictQueryWrapper = new QueryWrapper<>();
-        dictQueryWrapper.eq("parent_id",parentId);
+        dictQueryWrapper.eq("parent_id", parentId);
         List<Dict> dictList = baseMapper.selectList(dictQueryWrapper);
 //        填充hasChildren字段
         dictList.forEach(dict -> {
@@ -62,19 +98,26 @@ public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements Di
             boolean hasChildren = this.hasChildren(dict.getId());
             dict.setHasChildren(hasChildren);
         });
+        try {
+            log.info("将数据存入Redis");
+            redisTemplate.opsForValue().set("hfs:core:dictList:" + parentId,dictList,5, TimeUnit.MINUTES);
+        } catch (Exception e) {
+            log.error("Redis服务器异常:"+ ExceptionUtils.getStackTrace(e));
+        }
         return dictList;
     }
 
     /**
      * 判断当前id节点下是否有子节点
+     *
      * @param id
      * @return
      */
-    public boolean hasChildren(Long id){
+    public boolean hasChildren(Long id) {
         QueryWrapper<Dict> dictQueryWrapper = new QueryWrapper<>();
-        dictQueryWrapper.eq("parent_id",id);
+        dictQueryWrapper.eq("parent_id", id);
         Integer count = baseMapper.selectCount(dictQueryWrapper);
-        if (count>0){
+        if (count > 0) {
             return true;
         }
         return false;
